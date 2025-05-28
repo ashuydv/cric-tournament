30f7e4c7e02d11c769682d0f8af7e901c36c3aa1
ashutosh1333101 <ashutosh@bereej.com>
2025-05-28 23:30:46 +0530

    updated links !

--- components/social-media-links.tsx
+++ components/social-media-links.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const SocialMediaLinks = () => {
   const socialLinks = [
-    {
-      name: "Facebook",
-      href: "https://www.facebook.com/share/1AEtMsy5yM/?mibextid=wwXIfr",
-      path: `<path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />`,
-    },
+    // {
+    //   name: "Facebook",
+    //   href: "https://www.facebook.com/share/1AEtMsy5yM/?mibextid=wwXIfr",
+    //   path: `<path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />`,
+    // },
     {
       name: "Instagram",
       href: "https://www.instagram.com/_.runbhumi._/",
