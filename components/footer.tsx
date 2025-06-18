52a6e7a34136dd69198fa3fca6fc756c584e373f
ashutosh1333101 <ashutosh@bereej.com>
2025-06-18 12:20:49 +0530

    updated instagram link !

--- components/footer.tsx
+++ components/footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
     // },
     {
       name: "Instagram",
-      href: "https://www.instagram.com/_.runbhumi._/",
+      href: "https://www.instagram.com/therunbhumi/",
       icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
